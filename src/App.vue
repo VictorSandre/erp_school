e0daa59b1dacbc82e30431002a32ce76ba702b51
Victor SANDRE <victor.erdnas@gmail.com>
2020-08-30 17:07:25 +0200

    add Vuex to project

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,15 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import Vuex from 'vuex';
+import store from './Plugins/store/store'
+
+Vue.use(Vuex);
+
 export default {
   name: 'app',
+  store: store,
   data () {
     return {}
   }
